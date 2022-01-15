1818d3e4ca780c9fdf690aca9ae69b936d91dfb9
Sidharth Ballav <zid7139@gmail.com>
2022-01-15 13:30:08 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,12 @@
+public class multiply
+{
+  public static void main(String[] args)
+    {
+        int a = 4;
+        int b = 2;
+        System.out.println("The Product is = "+ (a*b));
+    }
+
+}
+    
+
